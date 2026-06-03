e7e2996c85607110b7b0bb1c8f61324614a4d0fa
kumatsegayie <kumatsegae@gmail.com>
2026-06-03 03:11:35 +0300

    Pull to refresh fixed

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
-// index.js
-// ----------------------------------------
-// App entry point.
-// Renders <App /> into the root DOM node.
-// Global styles are loaded here to apply
-// across the entire application.
-// ----------------------------------------
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
 
-// global styles
-import "./styles/global.css";
-import "./styles/variables.css";
-import "./styles/animations.css";
+const rootElement = document.getElementById("root");
 
-// i18n
-import "./i18n";
-import { installFetchFallback } from "./utils/installFetchFallback";
-
-installFetchFallback();
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
